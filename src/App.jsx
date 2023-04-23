6cfe023d6bac05e806f0700d5df2e42def65a838
h662Olbm <h662@olbm.app>
2023-04-24 07:38:00 +0900

    get my nft

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-950 text-white">
       <Header account={account} setAccount={setAccount} />
-      <Intro />
+      <Intro account={account} />
     </div>
   );
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,37 +1,75 @@
 import { FaChessRook } from "react-icons/fa";
-import { CONTRACT_ADDRESS } from "../web3.config";
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from "../web3.config";
+import { useEffect, useState } from "react";
+import Web3 from "web3";
 
 const ranNum = Math.floor(Math.random() * 1000) + 1;
 const imgSrc = `${process.env.REACT_APP_IMAGE_URL}/${ranNum}.png`;
 
-const Intro = () => {
+const web3 = new Web3(window.ethereum);
+const contract = new web3.eth.Contract(CONTRACT_ABI, CONTRACT_ADDRESS);
+
+const Intro = ({ account }) => {
+  const [myNfts, setMyNfts] = useState(0);
+
+  const getMyNfts = async () => {
+    try {
+      if (!contract || !account) return;
+
+      const response = await contract.methods.balanceOf(account).call();
+
+      console.log(response);
+
+      setMyNfts(response);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getMyNfts();
+  }, [contract, account]);
+
   return (
-    <div className="bg-gradient-to-b from-transparent via-red-400 to-gray-500  max-w-screen-xl mx-auto py-10 px-4 ">
-      <div className="relative w-20 h-20">
-        <img
-          className="w-20 h-20 rounded-full z-10 absolute top-0"
-          src={imgSrc}
-          alt="NFT"
-        />
-        <div className="w-20 h-20 rounded-full bg-white text-gray-950 absolute top-0 z-0 flex justify-center items-center text-xs">
-          Loading...
+    <div className="bg-gradient-to-b from-transparent  to-red-400 pt-10 px-4">
+      <div className="max-w-screen-xl mx-auto relative">
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-9xl opacity-40 text-yellow-300">
+          Da Den Bu
         </div>
-      </div>
-      <div className="mt-4 text-2xl font-bold flex items-center">
-        Da Den Bu
-        <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center ml-4 text-gray-950">
-          <FaChessRook size={16} />
+        <div className="relative w-20 h-20">
+          <img
+            className="w-20 h-20 rounded-full z-10 absolute top-0"
+            src={imgSrc}
+            alt="NFT"
+          />
+          <div className="w-20 h-20 rounded-full bg-white text-gray-950 absolute top-0 z-0 flex justify-center items-center text-xs">
+            Loading...
+          </div>
         </div>
-      </div>
-      <div className="mt-2 flex items-center text-gray-300">
-        by
-        <div className="text-main ml-2">{CONTRACT_ADDRESS}</div>
-      </div>
-      <div className="mt-2 flex items-center text-gray-300">
-        다덴부란(.env란 "environment"의 약어)은 소프트웨어 개발에서 자주
-        사용되는 파일 형식 중 하나입니다. 이 파일은 소프트웨어 개발자들이
-        프로젝트에서 사용되는 환경 변수(environment variable)를 저장하는 데
-        사용됩니다.
+        <div className="mt-4 text-2xl font-bold flex items-center">
+          Da Den Bu
+          <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center ml-4 text-gray-950">
+            <FaChessRook size={16} />
+          </div>
+        </div>
+        <div className="mt-2 flex items-center text-gray-300">
+          by
+          <div className="text-main ml-2">{CONTRACT_ADDRESS}</div>
+        </div>
+        <div className="mt-2 flex items-center text-gray-300">
+          다덴부란(.env란 "environment"의 약어)은 소프트웨어 개발에서 자주
+          사용되는 파일 형식 중 하나입니다. 이 파일은 소프트웨어 개발자들이
+          프로젝트에서 사용되는 환경 변수(environment variable)를 저장하는 데
+          사용됩니다.
+        </div>
+        <div className="py-4 flex text-center">
+          <div>
+            <div className="font-bold">1,000</div>
+            <div className="text-gray-300">총 NFT</div>
+          </div>
+          <div className="ml-4">
+            <div className="font-bold">{myNfts}</div>
+            <div className="text-gray-300">내 NFT</div>
+          </div>
+        </div>
       </div>
     </div>
   );
