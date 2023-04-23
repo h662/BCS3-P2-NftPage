602e30eecbca29376305c8c7a5a7b92f9af74dbd
h662Olbm <h662@olbm.app>
2023-04-23 18:07:59 +0900

    get coin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import { FaChessRook } from "react-icons/fa";
+import { AiFillHeart } from "react-icons/ai";
 import { BiWallet } from "react-icons/bi";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Header = ({ account, setAccount }) => {
+  const [coinPrice, setCoinPrice] = useState();
+
   const onClickAccount = async () => {
     try {
       const accounts = await window.ethereum.request({
@@ -14,29 +19,72 @@ const Header = ({ account, setAccount }) => {
     }
   };
 
+  const getPrice = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.upbit.com/v1/ticker?markets=KRW-BTC,%20KRW-ETH,%20KRW-MATIC"
+      );
+
+      setCoinPrice([
+        { symbol: "BTC", price: response.data[0].trade_price },
+        { symbol: "ETH", price: response.data[1].trade_price },
+        { symbol: "MATIC", price: response.data[2].trade_price },
+      ]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getPrice();
+  }, []);
+
   return (
     <header className="max-w-screen-xl mx-auto p-4 flex justify-between font-bold">
       <div className="text-main flex items-center">
         <FaChessRook size={28} />
         <div className="ml-1 text-xl">Ble-Chess</div>
       </div>
-      <div>
-        {account ? (
-          <div className="p-2 bg-gray-800 rounded-full">
-            {account.substring(0, 4)}...
-            {account.substring(account.length - 4)}
-          </div>
-        ) : (
-          <button
-            className="flex items-center p-2 bg-gray-800 rounded-full"
-            onClick={onClickAccount}
-          >
-            <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center">
-              <BiWallet />
+      <div className="flex items-center">
+        {coinPrice && (
+          <div className="flex text-gray-400">
+            <div>
+              {coinPrice[0].symbol}:{" "}
+              {(coinPrice[0].price / 1000).toLocaleString()}K₩
             </div>
-            <div className="ml-1">Connect</div>
-          </button>
+            <div className="ml-4">
+              {coinPrice[1].symbol}:{" "}
+              {(coinPrice[1].price / 1000).toLocaleString()}K₩
+            </div>
+            <div className="ml-4">
+              {coinPrice[2].symbol}:{" "}
+              {(coinPrice[2].price / 1000).toLocaleString()}K₩
+            </div>
+          </div>
         )}
+        <div className="ml-8">
+          {account ? (
+            <div className="p-2 bg-gray-800 rounded-full flex items-center">
+              <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center">
+                <AiFillHeart />
+              </div>
+              <div className="ml-1">
+                {account.substring(0, 4)}...
+                {account.substring(account.length - 4)}
+              </div>
+            </div>
+          ) : (
+            <button
+              className="flex items-center p-2 bg-gray-800 rounded-full"
+              onClick={onClickAccount}
+            >
+              <div className="bg-main w-6 h-6 rounded-full flex justify-center items-center">
+                <BiWallet />
+              </div>
+              <div className="ml-1">Connect</div>
+            </button>
+          )}
+        </div>
       </div>
     </header>
   );
